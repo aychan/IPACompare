e955c4a81fca1d19819aace94bc4b4f775f4cd9b
aychan <anthony.me.chan@gmail.com>
2016-03-08 16:07:33 -0500

    Initial Java Code Commit
    MARCH 8TH: initial file upload of the java project applicationIPA!
    
    To come: further transfer of files from other pre-existing personal
    project.

---
+++ applicationIPA/src/applicationipa/ApplicationIPA.java
@@ -0,0 +1,19 @@
+/**
+    Welcome to the IPACompare application main class!
+    If you are a contributer, please refrain from committing until 
+    your code is well debugged and tested within your java IDE.
+    Notice that this application is currently being made within NetBeans IDE 8.1,
+    so there is potential for IDE crossover hazards due to manifest.xml files.abstract 
+
+    Lets make cool stuff!
+**/
+package applicationipa;
+
+public class ApplicationIPA {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
